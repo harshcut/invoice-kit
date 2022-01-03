0452cd779758d76be836a37f0953af4d250d72c9
Harsh Karande <harshskarande@gmail.com>
2022-01-03 18:14:48 +0530

    chore: install fluent-ui with custom _document.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
 import type { AppProps } from 'next/app';
+import { initializeIcons, ThemeProvider } from '@fluentui/react';
+
+initializeIcons(undefined, { disableWarnings: true });
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 };
 
 export default MyApp;
