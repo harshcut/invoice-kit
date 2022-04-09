38a5edfed4afa8e1aab61546f5d75b8e15ca26e3
Harsh Karande <harshskarande@gmail.com>
2022-04-09 14:30:46 +0530

    fix(libs): modify method to get invoice serial

--- libs/getFiscalYear.ts
+++ libs/getFiscalYear.ts
@@ -1,6 +1,6 @@
 const getFiscalYear = (date?: Date): string => {
   const today = date ?? new Date();
-  if (today.getMonth() > 3)
+  if (today.getMonth() > 2)
     return today.getFullYear().toString() + '-' + (today.getFullYear() + 1).toString().slice(-2);
   return (today.getFullYear() - 1).toString() + '-' + today.getFullYear().toString().slice(-2);
 };
--- libs/getInvoiceSerial.ts
+++ libs/getInvoiceSerial.ts
@@ -2,7 +2,8 @@ import { getFiscalYear } from 'libs';
 
 const getInvoiceSerial = (invoice_id?: string): string => {
   if (!invoice_id) return `${getFiscalYear()}/${'1'.padStart(3, '0')}`;
-  const lastValue: number = Number(invoice_id.split('/')[1]) + 1;
+  const [fiscalYear, serialValue] = invoice_id.split('/');
+  const lastValue: number = getFiscalYear() === fiscalYear ? Number(serialValue) + 1 : 1;
   return `${getFiscalYear()}/${String(lastValue).padStart(3, '0')}`;
 };
 
